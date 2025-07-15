67f0af80cc02f206c8481cdae1c32588e6ff85c5
spandan3 <62089707+spandan3@users.noreply.github.com>
2025-07-15 17:14:36 +0400

    Modified app

--- backend/app.py
+++ backend/app.py
@@ -36,4 +36,4 @@ def classify_image():
     return jsonify({"class": str(pred_class), "confidence": round(confidence, 2)})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
